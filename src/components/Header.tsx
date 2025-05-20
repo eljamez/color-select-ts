bb74370fdbc4e0df062bd192d524b76be1264c45
James Hall <jamesh@aimi.fm>
2025-05-20 15:44:22 -0700

    added repo link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,12 @@ export const Header = memo(
   ({ currentlySelectedColors }: { currentlySelectedColors: Color[] }) => {
     return (
       <header className="Header">
+        <h1 className="Header__title">Color Selector</h1>
+        <h2 className="Header__subtitle">
+          <a href="https://github.com/eljamez/color-select-ts">
+            https://github.com/eljamez/color-select-ts
+          </a>
+        </h2>
         {currentlySelectedColors.length > 0 ? (
           <p className="Header__message">
             Your Currently Selected{" "}
